1a206e90751ee19548fdc2e5591e7d5973dcc801
SamGombs20 <gombajosh18@gmail.com>
2026-01-14 14:45:30 +0300

    Update:Auth screen development

---
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/view/AuthScreen.kt
@@ -0,0 +1,12 @@
+package org.work.project.presentation.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AuthScreen(){
+
+    Column {
+
+    }
+}
