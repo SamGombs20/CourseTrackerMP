ebf17262ddd0dd7af27f70a876d106046e57d751
SamGombs20 <gombajosh18@gmail.com>
2026-01-22 14:48:50 +0300

    Update:App Bar development

--- composeApp/src/commonMain/kotlin/org/work/project/presentation/view/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/view/MainScreen.kt
@@ -1,12 +1,15 @@
 package org.work.project.presentation.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -17,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -31,13 +35,17 @@ fun MainScreen(){
         topBar = {
             TopAppBar(
                 title = {
-                    Row {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(8 .dp)
+                    ) {
                         Text(
-                            text = "Course",
+                            text = "Hey,",
                             color = secondaryColor
                         )
                         Text(
-                            text = "Tracker"
+                            text = "Josh",
+                            fontWeight = FontWeight.Bold
                         )
                     }
                 },
