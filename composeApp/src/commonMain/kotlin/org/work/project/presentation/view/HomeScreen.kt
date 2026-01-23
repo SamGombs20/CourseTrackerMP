000faafb907e8a7978c97e73b9fba0ef16003b8c
SamGombs20 <gombajosh18@gmail.com>
2026-01-23 14:51:09 +0300

    Update:Home screen development

--- composeApp/src/commonMain/kotlin/org/work/project/presentation/view/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/view/HomeScreen.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
@@ -12,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -27,7 +31,13 @@ import org.work.project.presentation.ui.secondaryColor
 fun HomeScreen(){
     Column {
         Box(
-
+            modifier = Modifier.padding(16 .dp).fillMaxWidth().height(100 .dp).background(
+                brush = Brush.sweepGradient(
+                    colors = listOf(
+                        secondaryColor, Color.White
+                    )
+                )
+            )
         ){
             Row {
                 Row(
