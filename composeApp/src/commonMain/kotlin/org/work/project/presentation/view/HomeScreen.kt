b320fa9e7eb10023d5c62deb434db5799fa123e5
SamGombs20 <gombajosh18@gmail.com>
2026-01-23 14:30:27 +0300

    Update:Home screen development

--- composeApp/src/commonMain/kotlin/org/work/project/presentation/view/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/view/HomeScreen.kt
@@ -1,24 +1,35 @@
 package org.work.project.presentation.view
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontWeight
+import coursetrackermp.composeapp.generated.resources.Res
+import coursetrackermp.composeapp.generated.resources.active_courses
+import org.jetbrains.compose.resources.stringResource
 import org.work.project.presentation.ui.secondaryColor
 
 @Composable
 fun HomeScreen(){
     Column {
-        Row {
-            Text(
-                text = "Hello,"
-            )
-            Text(
-                text = "Name",
-                color = secondaryColor,
-                fontWeight = FontWeight.Bold
-            )
+        Box(
+
+        ){
+            Row {
+                Row {
+                    Column {
+
+                        Text(
+                            text = stringResource(Res.string.active_courses)
+                        )
+                        Text(
+                            text = "16"
+                        )
+                    }
+                }
+            }
         }
     }
 }
