d83520f76ff700f30698622175e6218641487e9b
SamGombs20 <gombajosh18@gmail.com>
2026-01-29 15:12:06 +0300

    Update:Delete course done

--- composeApp/src/commonMain/kotlin/org/work/project/presentation/components/CustomComponents.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/components/CustomComponents.kt
@@ -4,11 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.height
+
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -41,7 +40,7 @@ fun CustomTextField(
     trailingIcon: @Composable (() -> Unit)? = null
 ){
     Column(
-        modifier = Modifier.width(250 .dp)
+        modifier = Modifier.widthIn(max = 280 .dp)
     ) {
         OutlinedTextField(
             modifier =modifier,
