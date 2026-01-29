79b3de3de25cc829d2c846d4d78c1d6570b05a87
SamGombs20 <gombajosh18@gmail.com>
2026-01-29 13:08:48 +0300

    Update:Course details development

--- composeApp/src/commonMain/kotlin/org/work/project/presentation/components/CustomComponents.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/components/CustomComponents.kt
@@ -1,11 +1,14 @@
 package org.work.project.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -17,6 +20,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
 import org.work.project.presentation.ui.secondaryColor
 
 @Composable
@@ -63,7 +68,7 @@ fun DetailsTitle(title: String){
     Text(
         text = title,
         fontSize = 16 .sp,
-        fontWeight = FontWeight(600),
+        fontWeight = FontWeight(500),
         style = TextStyle(
             textDecoration = TextDecoration.Underline
         ),
@@ -77,4 +82,23 @@ fun DetailsText(details: String){
         text = details,
         fontSize = 13 .sp,
     )
+}
+@Composable
+fun TitleWithIcon(title: String, icon: DrawableResource){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16 .dp)
+    ){
+        Text(
+            text = title,
+            fontSize = 18 .sp,
+            fontWeight = FontWeight(600)
+        )
+        Icon(
+            painter = painterResource(icon),
+            contentDescription = title,
+            tint = secondaryColor,
+
+        )
+    }
 }
--- composeApp/src/commonMain/kotlin/org/work/project/presentation/view/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/view/HomeScreen.kt
@@ -1,7 +1,9 @@
 package org.work.project.presentation.view
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -17,17 +19,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coursetrackermp.composeapp.generated.resources.Res
+import coursetrackermp.composeapp.generated.resources.course_details
+import org.jetbrains.compose.resources.stringResource
 import org.work.project.model.Course
 import org.work.project.presentation.components.Banner
 import org.work.project.presentation.components.Course
 import org.work.project.presentation.components.CourseDetails
 import org.work.project.presentation.components.MainHeader
+import org.work.project.presentation.components.TitleWithIcon
 import org.work.project.presentation.ui.primaryColor
 import org.work.project.presentation.viewmodel.CourseViewModel
 
@@ -82,8 +89,11 @@ fun HomeScreen(courseViewModel: CourseViewModel= viewModel()){
                 )
             ) {
                 Column(
-                    modifier = Modifier.padding(16 .dp)
+                    modifier = Modifier.fillMaxWidth().padding(16 .dp)
                 ) {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+                        TitleWithIcon(stringResource(Res.string.course_details), Res.drawable)
+                    }
                     CourseDetails(selectedCourse!!)
                 }
             }
