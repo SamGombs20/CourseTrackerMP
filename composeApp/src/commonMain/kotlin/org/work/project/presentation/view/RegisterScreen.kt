75ed7fab9af7668c0f7fe6e89c0119ec99809517
SamGombs20 <gombajosh18@gmail.com>
2026-01-30 15:30:42 +0300

    Update:User authentication input validation

--- composeApp/src/commonMain/kotlin/org/work/project/presentation/view/RegisterScreen.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/view/RegisterScreen.kt
@@ -41,6 +41,7 @@ import coursetrackermp.composeapp.generated.resources.username_text
 import org.jetbrains.compose.resources.stringResource
 import org.work.project.navigation.Screen
 import org.work.project.presentation.components.CustomTextField
+import org.work.project.presentation.components.ErrorText
 import org.work.project.presentation.ui.secondaryColor
 
 @Composable
@@ -53,6 +54,56 @@ fun RegisterScreen (
     var username by remember { mutableStateOf("")}
     var password by remember { mutableStateOf("")}
     var confirmPassword by remember { mutableStateOf("") }
+    var firstNameError by remember { mutableStateOf("") }
+    var lastNameError by remember { mutableStateOf("") }
+    var usernameError by remember { mutableStateOf("") }
+    var passwordError by remember { mutableStateOf("") }
+    var confirmPasswordError by remember { mutableStateOf("") }
+
+    fun validateInputs(): Boolean{
+        var isValid = true
+        if(firstName.isEmpty()){
+            firstNameError = "First name cannot be empty"
+            isValid = false
+        }
+        else if(firstName.length<2){
+            firstNameError = "First name must be at lest 2 characters long"
+            isValid = false
+        }
+        if(lastName.isEmpty()){
+            lastNameError = "Last name cannot be empty"
+            isValid = false
+        }
+        else if(lastName.length<2){
+            lastNameError = "Must be at least 2 characters long"
+            isValid = false
+        }
+        if(username.isEmpty()){
+            usernameError ="Username cannot be empty"
+            isValid = false
+        }
+        else if (username.length<4){
+            usernameError = "Must be at least 4 characters long"
+            isValid = false
+        }
+        if(password.isEmpty()){
+            passwordError = "Password cannot be empty"
+            isValid = false
+        }
+        else if (password.length<6){
+            passwordError ="Must be at least 6 characters long"
+            isValid = false
+        }
+        if (confirmPassword.length<6){
+            confirmPasswordError = "Must be at least 6 characters long"
+            isValid = false
+        }
+        else if (confirmPassword != password){
+            confirmPasswordError ="Passwords do not match"
+            isValid = false
+        }
+        return isValid
+    }
     val scrollState = rememberScrollState()
     Column (
         modifier = Modifier.fillMaxSize().padding(16 .dp).verticalScroll(scrollState),
@@ -74,45 +125,70 @@ fun RegisterScreen (
             value = firstName,
             onChange = {
                 firstName = it
+                firstNameError=""
             },
+            isError = firstNameError.isNotEmpty(),
             label = stringResource(Res.string.first_name)
 
         )
+        if(firstNameError.isNotEmpty()){
+            ErrorText(firstNameError)
+        }
         Spacer(Modifier.height(8 .dp))
         CustomTextField(
             value = lastName,
             onChange = {
                 lastName = it
+                lastNameError =""
             },
+            isError = lastNameError.isNotEmpty(),
             label = stringResource(Res.string.last_name)
 
         )
+        if(lastNameError.isNotEmpty()){
+            ErrorText(lastNameError)
+        }
         Spacer(Modifier.height(8 .dp))
         CustomTextField(
             value = username,
             onChange = {
                 username = it
+                usernameError = ""
             },
+            isError = usernameError.isNotEmpty(),
             label = stringResource(Res.string.username_text)
 
         )
+        if(usernameError.isNotEmpty()){
+            ErrorText(usernameError)
+        }
         Spacer(Modifier.height(8 .dp))
         CustomTextField(
             value = password,
             onChange = {
                 password = it
+                passwordError=""
             },
+            isError = passwordError.isNotEmpty(),
             label = stringResource(Res.string.password_text)
         )
+        if(passwordError.isNotEmpty()){
+            ErrorText(passwordError)
+        }
         Spacer(Modifier.height(8 .dp))
         CustomTextField(
             value = confirmPassword,
             onChange = {
                 confirmPassword = it
+                confirmPasswordError=""
             },
+            isError = confirmPasswordError.isNotEmpty(),
             label = stringResource(Res.string.confirm)
 
         )
+        if(confirmPasswordError.isNotEmpty()){
+            ErrorText(confirmPasswordError)
+        }
         Spacer(Modifier.height(4 .dp))
 
         Row(
@@ -137,9 +213,11 @@ fun RegisterScreen (
         Spacer(Modifier.height(4 .dp))
         TextButton(
             onClick = {
-                navController.navigate(Screen.Main.route){
-                    popUpTo(Screen.Login.route){
-                        inclusive = true
+                if (validateInputs()){
+                    navController.navigate(Screen.Main.route){
+                        popUpTo(Screen.Login.route){
+                            inclusive = true
+                        }
                     }
                 }
             },
