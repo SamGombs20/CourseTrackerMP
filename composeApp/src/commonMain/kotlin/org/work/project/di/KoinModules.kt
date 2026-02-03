d503bdfc31fec10f7da7cce5234f7142cd55df52
SamGombs20 <gombajosh18@gmail.com>
2026-02-03 16:11:40 +0300

    Update:Dependency injection with koin

--- composeApp/src/commonMain/kotlin/org/work/project/di/KoinModules.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/di/KoinModules.kt
@@ -1,7 +1,6 @@
 package org.work.project.di
 
 import org.koin.dsl.module
-import org.koin.plugin.module.dsl.single
 import org.work.project.model.api.AuthApi
 import org.work.project.presentation.viewmodel.AuthViewModel
 import org.work.project.presentation.viewmodel.CourseViewModel
