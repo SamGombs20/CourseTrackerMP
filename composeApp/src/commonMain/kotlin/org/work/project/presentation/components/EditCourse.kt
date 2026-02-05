53d2e8b2c97966ab8f379da0471c4bc6102d3b35
SamGombs20 <gombajosh18@gmail.com>
2026-02-05 11:01:23 +0300

    Update:User courses edit

--- composeApp/src/commonMain/kotlin/org/work/project/presentation/components/EditCourse.kt
+++ composeApp/src/commonMain/kotlin/org/work/project/presentation/components/EditCourse.kt
@@ -263,7 +263,7 @@ fun EditCourse(onClose:(Boolean)-> Unit, course: Course, courseViewModel: Course
                                     description,
                                     status,
                                     if (startDate==null && course.startDate.isNotEmpty()) course.startDate else convertMillisToDate(startDate),
-                                    if (endDate == null && course.endDate.isNotEmpty()) course.endDate else convertMillisToDate(endDate),
+                                    if (endDate == null && course.endDate.length<5) course.endDate else convertMillisToDate(endDate),
                                     rating
                                 )
                             )
